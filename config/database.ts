1113440aa740eb9e5001a0d5d4613db60075af89
Remy-Poirier <remy.poirier92@gmail.com>
2024-02-14 11:08:33 +0100

    add ssl configuration

--- config/database.ts
+++ config/database.ts
@@ -12,6 +12,7 @@ const dbConfig = defineConfig({
         user: env.get('DB_USER'),
         password: env.get('DB_PASSWORD'),
         database: env.get('DB_DATABASE'),
+        ssl: Boolean(env.get('DB_SSL') ?? false),
       },
       migrations: {
         naturalSort: true,
