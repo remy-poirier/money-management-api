b3fbcce2ced1ffbb858e4a86cd45eeffb7a92159
Remy-Poirier <remy.poirier92@gmail.com>
2024-02-14 11:32:59 +0100

    Minor reorder of evolution

--- database/migrations/1707843063483_create_users_table.ts
+++ database/migrations/1707843063483_create_users_table.ts
@@ -4,9 +4,9 @@ export default class extends BaseSchema {
   protected tableName = 'users'
 
   async up() {
-    this.schema.createTable(this.tableName, (table) => {
-      this.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    this.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
 
+    this.schema.createTable(this.tableName, (table) => {
       table.uuid('id').defaultTo(this.raw('uuid_generate_v4()')).primary()
       table.string('full_name').nullable()
       table.string('email', 254).notNullable().unique()
