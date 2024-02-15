1fc985f8cff977f73fa00e208ed4d021abaa6992
Remy-Poirier <remy.poirier92@gmail.com>
2024-02-15 09:06:34 +0100

    Fix cors url

--- config/cors.ts
+++ config/cors.ts
@@ -9,7 +9,7 @@ import app from '@adonisjs/core/services/app'
  */
 const corsConfig = defineConfig({
   enabled: true,
-  origin: app.inProduction ? 'https://api.money-manager.tech' : '*',
+  origin: app.inProduction ? 'https://api.money-manager.tech/*' : '*',
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
   headers: true,
   exposeHeaders: [],
